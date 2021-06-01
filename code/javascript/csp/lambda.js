9e8ea3f48e1fc7fca777ecfa9b9c84ee31f86b73
rmrhz <rmrhz@tfwno.gf>
2021-06-01 14:15:32 +0900

    Add code/javascript/csp

---
+++ code/javascript/csp/lambda.js
@@ -0,0 +1,42 @@
+"use strict";
+const path = require("path");
+
+const formSpaceSeparatedList = (list) => list.join(" ");
+
+const selfSrcAllowList = [
+  //
+];
+
+const generateSelfSrcPolicy = () => {
+  return `'self' ${formSpaceSeparatedList(selfSrcAllowList)}`;
+};
+
+exports.handler = (event, context, callback) => {
+  const response = event.Records[0].cf.response;
+  const headers = response.headers;
+
+  headers["strict-transport-security"] = [
+    {
+      key: "Strict-Transport-Security",
+      value: "max-age=63072000; includeSubdomains; preload",
+    },
+  ];
+  headers["content-security-policy"] = [
+    {
+      key: "Content-Security-Policy",
+      value: `default-src 'self' ${generateSelfSrcPolicy()};`,
+    },
+  ];
+  headers["x-content-type-options"] = [
+    { key: "X-Content-Type-Options", value: "nosniff" },
+  ];
+  headers["x-frame-options"] = [{ key: "X-Frame-Options", value: "DENY" }];
+  headers["x-xss-protection"] = [
+    { key: "X-XSS-Protection", value: "1; mode=block" },
+  ];
+  headers["referrer-policy"] = [
+    { key: "Referrer-Policy", value: "same-origin" },
+  ];
+
+  callback(null, response);
+};
